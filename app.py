430351785b08930a83bf84fd49e766ee572d3e9d
bruxiz <143522268+bruxiz@users.noreply.github.com>
2023-09-02 23:03:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request
-import pprint
 
 app = Flask(__name__)
 
 def print_request_info(req):
     print("=== Headers ===")
-    pprint.pprint(dict(req.headers))
+    print(dict(req.headers))
     print("\n=== Method ===")
     print(req.method)
     print("\n=== Full Path ===")
@@ -15,14 +14,14 @@ def print_request_info(req):
     print("\n=== Data ===")
     print(req.data)
     print("\n=== Form ===")
-    pprint.pprint(req.form)
+    print(req.form)
     print("\n=== Args ===")
-    pprint.pprint(req.args)
+    print(req.args)
     print("\n=== JSON ===")
-    pprint.pprint(req.json)
+    print(req.json)
     print("===================")
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def hello_world():
     print_request_info(request)
 
@@ -32,4 +31,4 @@ def hello_world():
     return f'Hello, your IP is: {original_client_ip}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080), debug=True
