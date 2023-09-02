207df5fec216e5f32a830e0c222f361f812f9df4
bruxiz <143522268+bruxiz@users.noreply.github.com>
2023-09-02 22:21:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_ip():
-    user_ip = request.headers.get(request.environ['X-Forwarded-For'])
-    return user_ip, 200
+
+    x_forwarded_for = request.headers.get('X-Forwarded-For')
+    if x_forwarded_for:
+        user_ip = x_forwarded_for.split(',')[0]
+    else:
+        user_ip = request.remote_addr
+    return user_ip + " X-Forwarded-For header not found", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080, debug=DEBUG_MODE)
+    app.run(host='0.0.0.0', port=8080, debug=DEBUG_MODE)
